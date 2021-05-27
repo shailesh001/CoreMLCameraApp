fbd182952a83ba913bad2358691cd2c092ccd555
Shailesh Patel <shailesh001@mac.com>
2021-05-27 11:12:10 +0100

    Initial commit

--- CoreMLCameraApp/ViewController.swift
+++ CoreMLCameraApp/ViewController.swift
@@ -6,13 +6,53 @@
 //
 
 import UIKit
+import AVFoundation
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var videoFeed: UIView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var cameraOutput: AVCaptureOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    var captureSession: AVCaptureSession!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        useCamera()
     }
+    
+    func useCamera() {
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        
+        cameraOutput = AVCapturePhotoOutput()
+        
+        let device = AVCaptureDevice.default(for: AVMediaType.video)
+        if let input = try? AVCaptureDeviceInput(device: device!) {
+            if (captureSession.canAddInput(input)) {
+                captureSession.addInput(input)
+                
+                if (captureSession.canAddOutput(cameraOutput)) {
+                    captureSession.addOutput(cameraOutput)
+                }
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+                previewLayer.frame = videoFeed.bounds
+                videoFeed.layer.addSublayer(previewLayer)
+                captureSession.startRunning()
+            } else {
+                print("Could not get any input")
+            }
+        } else {
+            print("No video feed available")
+        }
+    }
+    
+    
 
 
 }
